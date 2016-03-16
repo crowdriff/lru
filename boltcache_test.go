e65e47926163847d3c731f582dd01278ec80b91f
ryanfowler <ryan.fowler19@gmail.com>
2016-03-16 18:27:52 -0400

    Add boltcache tests

---
+++ boltcache_test.go
@@ -0,0 +1,117 @@
+package lru
+
+import (
+	"bytes"
+	"strconv"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Boltcache", func() {
+
+	Context("openBoltDB", func() {
+
+		It("should return an error when attempting to open an invalid path", func() {
+			l := NewLRU(0, "///", "", nil)
+			defer closeBoltDB(l)
+			err := l.openBoltDB()
+			Ω(err).Should(HaveOccurred())
+		})
+	})
+
+	Context("fillCacheFromBolt", func() {
+
+		It("should attempt to fill the cache, but no data currently exists", func() {
+			l := NewLRU(0, "", "", nil)
+			defer l.Close()
+			err := l.openBoltDB()
+			Ω(err).ShouldNot(HaveOccurred())
+			Ω(l.items).Should(HaveLen(0))
+		})
+
+		It("should fill the cache with all data in the bolt database and delete items exceeding the capacity", func() {
+			// insert 1200 bytes into the bolt database
+			l := NewLRU(1000, "", "", nil)
+			err := l.Open()
+			Ω(err).ShouldNot(HaveOccurred())
+			for i := 0; i < 3; i++ {
+				err = l.putIntoBolt([]byte(strconv.Itoa(i)), make([]byte, 400))
+				Ω(err).ShouldNot(HaveOccurred())
+			}
+			closeBoltDB(l)
+
+			// attempt to open and fill LRU
+			l = newDefaultLRU()
+			defer closeBoltDB(l)
+			Ω(l.items).Should(HaveLen(2))
+			_, err = l.Get([]byte("3"))
+			Ω(err).Should(MatchError(errNoStore))
+		})
+	})
+
+	Context("getFromBolt", func() {
+
+		It("should return nil when trying to retrieve a key that doesn't exist", func() {
+			l := newDefaultLRU()
+			defer closeBoltDB(l)
+			v := l.getFromBolt([]byte("key"))
+			Ω(v).Should(BeNil())
+		})
+
+		It("should return the value from bolt", func() {
+			l := newDefaultLRU()
+			defer closeBoltDB(l)
+			err := l.putIntoBolt([]byte("key"), []byte("value"))
+			Ω(err).ShouldNot(HaveOccurred())
+			v := l.getFromBolt([]byte("key"))
+			Ω(bytes.Equal(v, []byte("value"))).Should(BeTrue())
+		})
+	})
+
+	Context("emptyBolt", func() {
+
+		It("should empty the bolt database's bucket", func() {
+			// create LRU and insert key
+			l := newDefaultLRU()
+			defer closeBoltDB(l)
+			err := l.putIntoBolt([]byte("key"), []byte("value"))
+			Ω(err).ShouldNot(HaveOccurred())
+			v := l.getFromBolt([]byte("key"))
+			Ω(string(v)).Should(Equal("value"))
+
+			// empty the database
+			err = l.emptyBolt()
+			Ω(err).ShouldNot(HaveOccurred())
+			v = l.getFromBolt([]byte("key"))
+			Ω(v).Should(BeNil())
+		})
+	})
+
+	Context("deleteFromBolt", func() {
+
+		It("should delete the provided keys from the bolt database", func() {
+			// create LRU and insert keys
+			l := newDefaultLRU()
+			defer closeBoltDB(l)
+			var toRemove [][]byte
+			for i := 0; i < 4; i++ {
+				key := []byte(strconv.Itoa(i))
+				toRemove = append(toRemove, key)
+				err := l.putIntoBolt(key, []byte("value"))
+				Ω(err).ShouldNot(HaveOccurred())
+			}
+
+			// delete 3 from bolt
+			err := l.deleteFromBolt(toRemove[:3])
+			Ω(err).ShouldNot(HaveOccurred())
+			for i := 0; i < 3; i++ {
+				v := l.getFromBolt(toRemove[i])
+				Ω(v).Should(BeNil())
+			}
+			v := l.getFromBolt(toRemove[3])
+			Ω(v).ShouldNot(BeNil())
+			Ω(string(v)).Should(Equal("value"))
+		})
+	})
+})
--- lru_suite_test.go
+++ lru_suite_test.go
@@ -1,13 +1,35 @@
-package lru_test
+package lru
 
 import (
+	"testing"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-
-	"testing"
 )
 
 func TestLru(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Lru Suite")
+}
+
+var _ = BeforeEach(func() {
+	l := NewLRU(0, "", "", nil)
+	err := l.Open()
+	Ω(err).ShouldNot(HaveOccurred())
+	defer l.Close()
+	err = l.emptyBolt()
+	Ω(err).ShouldNot(HaveOccurred())
+})
+
+func newDefaultLRU() *LRU {
+	l := NewLRU(0, "", "", nil)
+	err := l.Open()
+	Ω(err).ShouldNot(HaveOccurred())
+	return l
+}
+
+func closeBoltDB(l *LRU) {
+	if l.cache != nil {
+		l.Close()
+	}
 }
